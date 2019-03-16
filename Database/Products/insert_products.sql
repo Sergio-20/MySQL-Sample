c8b331507a3b90c9de7bc10f70c995166c901f4a
sergio-20 <nubesgladius20@gmail.com>
2019-03-16 11:25:31 -0700

    Ways to select data

--- Database/Products/insert_products.sql
+++ Database/Products/insert_products.sql
@@ -1,3 +1,5 @@
 use myStore;
 
 INSERT INTO products(name, manufacturer, quantity) VALUES(jackets, adidas, 55);
+INSERT INTO products(name, manufacturer, quantity) VALUES(shoes, nike, 16);
+INSERT INTO products(name, manufacturer, quantity) VALUES(shirts, polo, 28);
---
+++ Database/Products/select_products.sql
@@ -0,0 +1,14 @@
+SELECT * FROM myStore.users WHERE f_name LIKE '%J%';
+
+SELECT * FROM myStore.users WHERE f_name NOT LIKE '%oe%';
+
+SELECT * FROM myStore.products WHERE name = 'shoes' AND quantity > 10;
+
+SELECT f_name,
+    CASE
+      WHEN f_name = 'Jane' THEN 'ARE YOU JANE DOE!?'
+      WHEN f_name = 'Joe'  THEN 'ARE YOU THE, JOHN DOE!?'
+      ELSE 'WHO ARE YOU?'
+    END AS name_detector;
+
+SELECT name FROM myStore.products WHERE name IN('adidas','nike');
