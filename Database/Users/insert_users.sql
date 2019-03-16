516c51f0b6fa919a1267ba606dff6d4841cc2467
sergio-20 <nubesgladius20@gmail.com>
2019-03-16 14:00:44 -0700

    MySQL Functions Examples

--- Database/Users/insert_users.sql
+++ Database/Users/insert_users.sql
@@ -3,3 +3,7 @@ use myStore;
 INSERT INTO users(f_name, l_name, email, username, pwd) VALUES("John", "Doe", "jdoe@example.com", "jDoe123", "password123");
 
 INSERT INTO users(f_name, l_name, email, username, pwd) VALUES("Jane", "Doe", "jadoe@example.com", "jDoe456", "password456");
+
+INSERT INTO users(f_name, l_name, email, username, pwd) VALUES("Chris", "Williams", "cwilliams@example.com", "cwill123", "password789");
+
+INSERT INTO users(f_name, l_name, email, username, pwd) VALUES("Jose", "Rodriguez", "jrod123@example.com", "rod453", "password255");
---
+++ Database/mysql_functions/mysql_functions.sql
@@ -0,0 +1,25 @@
+SELECT COUNT(*) FROM myStore.users WHERE f_name = '%J%';
+
+SELECT COUNT(DISTINCT username) FROM myStore.users WHERE username = "%J%" AND id > 1;
+
+SELECT MIN(quantity) FROM myStore.products;
+
+SELECT MAX(quantity) FROM myStore.products;
+
+SELECT SUM(quantity) FROM myStore.quantity;
+
+SELECT AVG(quantity) FROM myStore.quantity;
+
+SELECT f_name, l_name FROM myStore.users GROUP BY l_name;
+
+SELECT f_name, l_name FROM myStore.users ORDER BY l_name DESC;
+
+SELECT CONCAT(f_name, ' ', l_name) AS user_info FROM myStore.users;
+
+SELECT REPLACE(manufacturer, 'nike', 'puma') manufacturer_change FROM myStore.products;
+
+SELECT LOWER(f_name, l_name) AS lowercase_name FROM myStore.users;
+
+SELECT UPPER(f_name, l_name) AS uppercase_name FROM myStore.users;
+
+SHOW tables;
